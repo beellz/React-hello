9ead8ac782c835ba9f5a61081c13ed4776d0231c
beellz <belal20795@gmail.com>
2020-05-21 23:57:41 +0530

    added inline style

--- src/index.js
+++ src/index.js
@@ -8,11 +8,18 @@ const year = CurrentDate.getFullYear();
 console.log(year);
 
 const upimage = "https://picsum.photos/200"
+const CustomColor = {
+  color: "red",
+  fontSize : "26px"
+}
+
+CustomColor.color = "green"
+
 
 ReactDOM.render(
   <div>
     <h1 className= "heading" contentEditable="true" spellCheck="false"> hello </h1>
-    <p> created by {name}</p>
+    <p style={CustomColor}> created by {name}</p>
 <p> copyright {year}</p>
  <img src={upimage + "?grayscale"}/>
   </div>
